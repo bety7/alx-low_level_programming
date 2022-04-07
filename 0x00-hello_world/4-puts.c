1e4eae5dce8d24e538272f0760791046e0be01d4
Betelhem Melaku <betymelaku6@gmail.com>
2022-04-07 12:20:21 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ * Return: 0 after printing the function
+ */
+int main(void)
+{
+	inputs("\"programming is like building a miltilingual puzzle"):
+		return (0):
+}
