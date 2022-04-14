5a2b536efea07cd22952c6b4f96d807962e1a6e6
Betelhem Melaku <betymelaku6@gmail.com>
2022-04-14 10:13:54 -0700

    Copy this main file. Comment ou

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
- * main - causes an infinite loop
- * Return: 0
- */
+* main - causes an infinite loop
+* Return: 0
+*/
+
 int main(void)
 {
 int i;
@@ -10,14 +12,13 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
+/**
+*while (i < 10)
+*{
+*putchar(i);
+}
+*/
+printf("Infinite loop avoided! \\o/\n");
 
-/*
- * while (1 < 10)
- *{
- *Infinite loop - No increase of variable
- *putchar(i);
- }
- */
-printf("Infinite loop avoided! \\0/\n");
 return (0);
 }
