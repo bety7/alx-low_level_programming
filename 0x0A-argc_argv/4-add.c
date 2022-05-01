cea083ebe880bc163a89bacfbf553286e4a23dc3
Betelhem Melaku <betymelaku6@gmail.com>
2022-05-01 08:21:17 -0700

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,36 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <ctype.h>
+
 /**
- * main - Entry Point
- * @argc: arguments
- * @argv: array pointing to arguments
- * Return: 0
+ * main - prints buffer in hexa
+ * @argc: he number of command line arguments
+ * @argv: An array containing the program command line arguments.
+ *
+ * Return: Nothing.
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int sum = 0, i, j;
 
-	if (argc < 1)
-		return (0);
-
-	for (i = 1; i < argc; i++)
+	if (argc > 0)
 	{
-		if (!atoi(argv[i]))
+		for (i = 1; i < argc; i++)
 		{
-			printf("%s\n", "Error");
-			return (1);
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (!(isdigit(argv[i][j])))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
 		}
-		sum += atoi(argv[i]);
+			printf("%d\n", sum);
 	}
-	printf("%d\n", sum);
-
+	else
+	printf("0\n");
 	return (0);
 }
